ad98d53deb3fd6a6af40b068741bb91bd97579bf
Thomas <urufu.mion@gmail.com>
2019-05-10 14:03:27 +0200

    new shop

--- WeaponsOfChaos/shop.cpp
+++ WeaponsOfChaos/shop.cpp
@@ -1,12 +1,43 @@
 #include "stdafx.h"
-#include "shop.h"
+#include "Shop.h"
 
 
-shop::shop()
+void Shop::refresh()
 {
 }
 
+void Shop::choseItem()
+{
+}
 
-shop::~shop()
+void Shop::addItem()
+{
+}
+
+void Shop::unavailableItem(int item)
+{
+}
+
+void Shop::availableItem(int item)
+{
+}
+
+void Shop::drawTo(sf::RenderWindow & window)
+{
+	window.draw(shop);
+}
+
+void Shop::setPos(sf::Vector2f newPos)
+{
+	shop.setPosition(newPos);
+}
+
+Shop::Shop(sf::Vector2f size)
+{
+	shop.setSize(size);
+}
+
+
+Shop::~Shop()
 {
 }
--- WeaponsOfChaos/shop.h
+++ WeaponsOfChaos/shop.h
@@ -1,13 +1,19 @@
 #pragma once
-class shop
+
+#include <SFML\Graphics.hpp>
+class Shop
 {
+private :
+	sf::RectangleShape shop;
 public:
 	void refresh();
 	void choseItem();
 	void addItem();
 	void unavailableItem(int item); // When player had already buy a item or don't have money to buy it.
 	void availableItem(int item); // When player can buy a item.
-	shop();
-	~shop();
+	void drawTo(sf::RenderWindow &window);
+	void setPos(sf::Vector2f newPos);
+	Shop(sf::Vector2f size);
+	~Shop();
 };
 

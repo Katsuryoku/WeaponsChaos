fd38406bba276b136345a5aa418f744eb0b2914c
Thomas <urufu.mion@gmail.com>
2019-05-21 17:30:10 +0200

    update screenVideo

---
+++ WeaponsOfChaos/ScreenVideo.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "opencv2/opencv.hpp"
+
+using namespace cv;
+
+class ScreenVideo
+{
+private:
+	VideoCapture cap= VideoCapture(0);
+	bool isScreen = false;
+	cv::Mat frameRGB;
+	cv::Mat frame;
+	sf::Sprite sprite;
+	cv::Mat frameRGBA;
+	sf::Image image;
+	sf::Texture texture;
+	bool isZoneRight = false;
+	bool zone = false;
+public:
+	bool getIsScreen() { return isScreen; }
+	void setIsScreen(bool menu) { isScreen = menu; }
+	bool getZone() { return zone; }
+	void setZone(bool menu) { zone = menu; }
+	bool getIsZoneRight() { return isZoneRight; }
+	void setIsZoneRight(bool menu) { isZoneRight = menu; }
+	Mat captureImage();
+	void closeCam();
+	ScreenVideo();
+	bool takeVideo();
+	void drawTo(sf::RenderWindow & window);
+	~ScreenVideo();
+};
+
